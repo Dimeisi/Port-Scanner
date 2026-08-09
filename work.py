8568104dca572b4ea6852586bd844bc6269a2caf
Dimeisi <146081511+Dimeisi@users.noreply.github.com>
2026-08-09 16:56:59 +0500

    feat: test socket and build first scan.port

--- work.py
+++ work.py
@@ -4,3 +4,12 @@ import threading
 import concurrent.futures
 import json
 import time
+
+host = "37.99.26.101"
+port = 80
+
+# First version scan port
+def scan_port(host, port):
+    pass
+
+print(scan_port(host, port))
